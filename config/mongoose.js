1949a5559238e3837e23412133fbf884380c5387
samerr07 <112320710+samerr07@users.noreply.github.com>
2022-12-19 10:32:35 +0530

    initial

---
+++ config/mongoose.js
@@ -0,0 +1,23 @@
+// requiring the library
+const mongoose = require("mongoose");
+
+//connecting to the database
+mongoose.connect(
+  "mongodb://localhost:27017/PlacementCell"
+);
+
+
+
+// acquiring the connection to check if it is succesfull
+const db = mongoose.connection;
+
+// checking for the error
+db.on("error", console.error.bind(console, "error in connecting the database"));
+
+// up and running then print the statement
+db.once("open", () => {
+  console.log("succesfully connected to database");
+});
+
+// exporting the connection
+module.exports = db;
